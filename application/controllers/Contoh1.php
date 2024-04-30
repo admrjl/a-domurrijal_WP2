15ea38ff5d8c9407cbae81763ba0d96da5b5a099
admrjl <15220676@bsi.ac.id>
2024-04-30 16:45:12 +0700

    pertemuan 5 dan tugas 4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya herman firmansyah <br>
+        saya tinggal di purnajaya 1 jalur 2 no.28, <br>
+        saya adalah seorang mahasiswa ubsi semester 4,<br>
+         dan ini adala tugas pertama makul WP2";
+    }
+}
